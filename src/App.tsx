ee127a1fc1692c0f36a073bfb64b28dd50029221
Yura <kovalevichyura@gmail.com>
2025-10-18 15:42:43 +0300

    feat: add Header component with navigation and hover effects

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/layout/Header';
+import './App.css';
+import AppRouter from './components/common/AppRouter';
+
 function App() {
-  return <>Hello world</>;
+  return (
+    <div className="wrapper">
+      <BrowserRouter>
+        <Header />
+        <AppRouter />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
