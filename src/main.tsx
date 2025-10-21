620375d5f05e9dd8fa36373fc75af40e0abfdcbb
Yura <kovalevichyura@gmail.com>
2025-10-21 17:44:56 +0300

    feat: implement error boundary for error handling

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { FavoritesProvider } from './components/common/FavoritesContext.tsx';
+import ErrorBoundary from './components/common/ErrorBoundary.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <FavoritesProvider>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </FavoritesProvider>
 );
